f85fdce815d4a56b9aef9a6e34a3ba42c00525b9
Niccolò Zona <51946697+damn-fox@users.noreply.github.com>
2020-02-13 13:11:16 +0100

    Update connectionDB.js

--- nodeMySql/connectionDB.js
+++ nodeMySql/connectionDB.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 exports.createConnectionDB = () => {
     var con = mysql.createConnection({
         host: "localhost",
-        user: "niccodb",
-        password: "taglierino2000",
-        database: 'sign_fix'
+        user: "",
+        password: "",
+        database: ''
       });
     return con;
   }
