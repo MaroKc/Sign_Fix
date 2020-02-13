ae05ca90aef470e47035b96190db81e85f9996a7
damn-fox <zonaniccolo92@gmail.com>
2020-02-13 13:02:17 +0100

    niccolò added nodemysql folder

---
+++ nodeMySql/connectionDB.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+exports.createConnectionDB = () => {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "niccodb",
+        password: "taglierino2000",
+        database: 'sign_fix'
+      });
+    return con;
+  }
