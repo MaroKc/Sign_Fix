ff77273a96085f499171b0738431b890a54d61dc
MeMarock <daniele.marocchi97@gmail.com>
2020-02-20 19:19:27 +0100

    Reso dinamico lo sfondo

--- template/src/views/Classi/Classi.js
+++ template/src/views/Classi/Classi.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Button, UncontrolledCollapse, CardTitle, CardText, CardImg, CardSubtitle, CardImgOverlay } from 'reactstrap';
+import {Col, Row } from 'reactstrap';
 import { Line } from 'react-chartjs-2';
-import Widget05 from './Widget05';
-import Collapses from '../Base/Collapses/Collapses';
+import CardClassi from './CardClassi';
 import axios from 'axios';
+
+
+const rdn = () => {
+
+  const numb = []
+  for (let l = 0; l < 7; l++)
+    numb.push(Math.floor(Math.random() * (100 - 1) + 1));
+
+  return ({ data : numb, label : ''})
+}
 
 // Brand Card Chart
 const makeSocialBoxData = (dataSetNo) => {
-  const socialBoxData = [
-    { data: [65, 59, 84, 84, 51, 55, 40], label: '' },
-    { data: [1, 13, 9, 17, 34, 41, 38], label: '' },
-  ];
 
-  const dataset = socialBoxData[dataSetNo];
+  const dataset = rdn();
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['', '', '', '', '', '', ''],
     datasets: [
       {
         backgroundColor: 'rgba(255,255,255,.1)',
@@ -30,6 +35,9 @@ const makeSocialBoxData = (dataSetNo) => {
 };
 
 const socialChartOpts = {
+  tooltips: {
+    enabled: false
+  },
   responsive: true,
   maintainAspectRatio: false,
   legend: {
@@ -55,9 +63,6 @@ const socialChartOpts = {
   },
 };
 
-// Collapse const
-
-
 class Classi extends Component {
 
   state = {
@@ -80,53 +85,47 @@ class Classi extends Component {
 
       items.push(
         
-        <Row className="mx-5">
+        <Row key={i} className="mx-lg-5">
           <Col xs={12} sm={12} md={6}>
             <div onClick={() => alert("Hello from here")}>
-              <Widget05 dataBox={() => ({ variant: this.state.corsi[i].name, anno: this.state.corsi[i].start_year + "-" + this.state.corsi[i].end_year })} >
-                <div key={i} className="chart-wrapper">
+              <CardClassi dataBox={() => ({ variant: this.state.corsi[i].name, anno: this.state.corsi[i].start_year + "-" + this.state.corsi[i].end_year })} >
+                <div key={this.state.corsi[i].id} className="chart-wrapper">
 
                   <Line data={makeSocialBoxData(0)} options={socialChartOpts} height={90} />
                 </div>
-              </Widget05>
+              </CardClassi>
             </div>
           </Col>
 
           <Col xs={12} sm={12} md={6}>
             <div onClick={() => alert("Hello from here")}>
-              <Widget05 dataBox={() => ({ variant: this.state.corsi[i+1].name, anno: this.state.corsi[i+1].start_year + "-" + this.state.corsi[i+1].end_year })} >
-                <div key={i} className="chart-wrapper">
+              <CardClassi dataBox={() => ({ variant: this.state.corsi[i+1].name, anno: this.state.corsi[i+1].start_year + "-" + this.state.corsi[i+1].end_year })} >
+                <div key={this.state.corsi[i+1].id} className="chart-wrapper">
 
                   <Line data={makeSocialBoxData(0)} options={socialChartOpts} height={90} />
                 </div>
-              </Widget05>
+              </CardClassi>
             </div>
           </Col>
         </Row>
       )
-      console.log(this.state.corsi[i].name)
-      console.log(this.state.corsi[i+1].name)
-
     }
 
 
     return (
       <div className="animated fadeIn">
-        <CardBody body outline>
 
           {items}
 
-         { this.state.corsi.map(corso => 
+         {/* this.state.corsi.map(corso => 
          <div>
          <Widget05 dataBox={() => ({ variant: corso.name, anno: corso.start_year + "-" + corso.end_year })} > 
          </Widget05>
          <li key={corso.id}>{corso.name}</li>
 
          </div>
-         )}
+         )*/}
 
-
-        </CardBody>
       </div>
     );
   }
