17d7b07bdead0cd85476d2be9ef0a610288e0523
damn-fox <zonaniccolo92@gmail.com>
2020-02-23 21:08:11 +0100

    studenti finitissima

--- template/src/views/Gestione/InfoStudente.js
+++ template/src/views/Gestione/InfoStudente.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Col, Row, CardHeader, Modal, ModalBody, ModalFooter, ModalHeader, Button } from 'reactstrap';
+import React from 'react'
+import { Card, Col, Row, CardHeader, Modal, ModalBody, ModalFooter, ModalHeader, Button, Input, Table } from 'reactstrap';
 import axios from 'axios'
 
 
@@ -37,36 +37,81 @@ class infoStudente extends React.Component {
       [name]:val,
       });
   }
-  
-  displayTable = () => {
-    this.setState({
-      displayCard: !this.state.displayCard
-    })
-  }
+
   changeInfo =(changeInfo) => {
     if (!changeInfo){
       return(
-        <div  className="m-5 ">
-          <h5 id="infoEmail">{this.state.email}</h5>
-          <h5 id="infoNome">{this.state.firstName}</h5>
-          <h5 id="infoCognome">{this.state.lastName}</h5>
-          <h5 id="infoResidenza">{this.state.residence}</h5>
-          <h5 id="infoDataNascita">{this.state.dateOfBirth}</h5>
-          <h5 id="infoCodice">{this.state.fiscalCode}</h5>
-          
-        </div>
+        <Table borderless responsive>
+          <tbody>
+            <tr>
+              <td><h5>Email:</h5></td>
+              <td><h5 id="infoEmail">{this.state.email}</h5></td>
+            </tr>
+            <tr>
+              <td><h5>Nome:</h5></td>
+              <td><h5 id="infoNome">{this.state.firstName}</h5></td>
+            </tr>
+            <tr>
+              <td><h5>Cognome:</h5></td>
+              <td><h5 id="infoCognome">{this.state.lastName}</h5></td>
+            </tr>
+            <tr>
+              <td><h5>Residenza:</h5></td>
+              <td><h5 id="infoResidenza">{this.state.residence}</h5></td>
+            </tr>
+            <tr>
+              <td><h5>Data di nascita:</h5></td>
+              <td><h5 id="infoDataNascita">{this.state.dateOfBirth}</h5></td>
+            </tr>
+            <tr>
+              <td><h5>Codice fiscale:</h5></td>
+              <td><h5 id="infoCodice">{this.state.fiscalCode}</h5></td>
+            </tr>
+          </tbody>
+        </Table>
+
       )
     }
     else{
+      const lettere = /^[a-zA-Z]*$/;
+ 
+      const {firstName, lastName, residence, fiscalCode, dateOfBirth} = this.state
+      const validationFirstName = firstName.length > 2 && lettere.test(firstName)
+      const validationLastName = lastName.length > 2 && lettere.test(lastName)
+      const validationResidence = residence.length > 2 
+      const validationDateOfBirth = dateOfBirth.length == 10 
+      const validationFiscalCode = fiscalCode.length == 12
+     
       return(
-        <div  className="m-5 w-auto">
-          <h5> {this.state.email} </h5>
-          <input id="infoNome"  name='firstName'  onChange={this.handleChange}  value={this.state.firstName}  required minLength="3" />
-          <input id="infoCognome" name='lastName' onChange={this.handleChange} value={this.state.lastName} required />
-          <input id="infoResidenza" name='residence' onChange={this.handleChange} value={this.state.residence} required />
-          <input id="infoDataNascita" name='dateOfBirth' onChange={this.handleChange} value={this.state.dateOfBirth} required />
-          <input id="infoCodice" name='fiscalCode' onChange={this.handleChange} value={this.state.fiscalCode} required />
-        </div>
+        
+        <Table borderless responsive>
+        <tbody>
+          <tr>
+            <td><h5>Email:</h5></td>
+            <td><h5 id="infoEmail">{this.state.email}</h5></td>
+          </tr>
+          <tr>
+            <td><h5>Nome:</h5></td>
+            <td><Input id="infoNome"  name='firstName'  onChange={this.handleChange}  value={this.state.firstName} valid={validationFirstName} invalid={!validationFirstName} /></td>
+          </tr>
+          <tr>
+            <td><h5>Cognome:</h5></td>
+            <td><Input id="infoCognome" name='lastName' onChange={this.handleChange} value={this.state.lastName} valid={validationLastName} invalid={!validationLastName}/></td>
+          </tr>
+          <tr>
+            <td><h5>Residenza:</h5></td>
+            <td><Input id="infoResidenza" name='residence' onChange={this.handleChange} value={this.state.residence} valid={validationResidence} invalid={!validationResidence}/></td>
+          </tr>
+          <tr>
+            <td><h5>Data di nascita:</h5></td>
+            <td><Input id="infoDataNascita" name='dateOfBirth' onChange={this.handleChange} value={this.state.dateOfBirth} valid={validationDateOfBirth} invalid={!validationDateOfBirth}/></td>
+          </tr>
+          <tr>
+            <td><h5>Codice fiscale:</h5></td>
+            <td><Input id="infoCodice" name='fiscalCode' onChange={this.handleChange} value={this.state.fiscalCode} valid={validationFiscalCode} invalid={!validationFiscalCode}/></td>
+          </tr>
+        </tbody>
+      </Table>
       )
     }
   }
@@ -110,21 +155,54 @@ class infoStudente extends React.Component {
        this.refresh();
    }
 
-   renderButtons = (changeInfo) => {
-    if(!changeInfo){
+  renderButtons = (changeInfo) => {
+    const { firstName, lastName, residence, fiscalCode, dateOfBirth } = this.state
+    const validationInput = firstName.length > 3 && lastName.length > 2 && residence.length > 2 && fiscalCode.length == 12 && dateOfBirth.length == 10
+    if (this.state.ritirato === 0) {
+      if (!changeInfo) {
+        return (
+          <>
+            <Button id="modifica" color="ghost-dark" onClick={this.onclickModifyState}><i className="cui-settings icons font-2xl d-block mt-4"></i>&nbsp;<p>Modifica</p></Button>
+            <Button color="ghost-danger" onClick={this.toggleWarning} className="mr-1"><i className="cui-user-unfollow icons font-2xl d-block mt-4"></i>&nbsp;<p>Ritira</p>  </Button>
+          </>
+        )
+      } else {
+        return (
+          <>
+            <Button outline color="dark" onClick={this.callForUpdate} disabled={!validationInput}> <i className="cui-check icons font-2xl d-block mt-2"></i>conferma <br /> modifica</Button>
+            {/* <Button outline color="dark" onClick={this.onclickModifyState}>torna <br /> indietro</Button> */}
+          </>
+        )
+      }
+    } else {
       return (
         <>
-        <Button id="modifica" color="ghost-info" onClick={this.onclickModifyState}><i className="cui-settings icons font-2xl d-block mt-4"></i>&nbsp;<p>Modifica</p></Button>
-        </>
-      )
-    }else{
-      return (
-        <>
-          <Button color="ghost-info" onClick={this.callForUpdate}>conferma <br /> modifica</Button>
         </>
       )
     }
   }
+
+  openModal = () => {
+    return(
+      <Modal isOpen={this.state.warning} toggle={this.toggleWarning}
+        className={'modal-danger ' + this.props.className}>
+        <ModalHeader toggle={this.toggleWarning}>WARNING</ModalHeader>
+        <ModalBody>
+          <div className="text-center">
+            <b>{this.state.firstName} {this.state.lastName}</b> verrà inserito tra gli studenti <b>ritirati</b>
+            <h5 >Procedere?</h5>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" onClick={this.callForRetire}>Continua</Button>{' '}
+          <Button color="secondary" onClick={this.toggleWarning}>Cancella</Button>
+        </ModalFooter>
+      </Modal>
+    )
+  }
+
+  
+
   render() {
 
     return (
@@ -134,41 +212,17 @@ class infoStudente extends React.Component {
           <b>{this.state.firstName}  {this.state.lastName}</b>
           </CardHeader>
           <Row>
-            <Col >
-              <div className="text-left font-weight-bold m-5 w-100">
-                <h5>Email: </h5>
-                <h5>Nome: </h5>
-                <h5>Cognome: </h5>
-                <h5>Residenza: </h5>
-                <h5>Data di nascita: </h5>
-                <h5>Codice fiscale: </h5>
-              </div>
+            <Col>
+            <div className="m-5" xs="auto">
+            {this.changeInfo(this.state.changeInfo)}
+            </div>        
             </Col>
-            <Col >
-              {this.changeInfo(this.state.changeInfo)}
-            </Col>
-            <Col xs="auto">
-              <div className="d-flex justify-content-center m-5">
+            <Col xs="auto" className="my-auto m-5">           
                 {this.renderButtons(this.state.changeInfo)}
-                <Button color="ghost-danger" onClick={this.toggleWarning} className="mr-1"><i className="cui-user-unfollow icons font-2xl d-block mt-4"></i>&nbsp;<p>Ritira</p>  </Button>
-              </div>
-              <Modal isOpen={this.state.warning} toggle={this.toggleWarning}
-                className={'modal-warning ' + this.props.className}>
-                <ModalHeader toggle={this.toggleWarning}>Modal title</ModalHeader>
-                <ModalBody>
-                  <div className="text-center">
-                    <b>{this.state.firstName} {this.state.lastName}</b> verrà inserito tra gli studenti <b>ritirati</b>
-                    <h5 >Procedere?</h5>
-                  </div>
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="warning" onClick={this.callForRetire}>Continua</Button>{' '}
-                  <Button color="secondary" onClick={this.toggleWarning}>Cancella</Button>
-                </ModalFooter>
-              </Modal>
+              {this.openModal()}
             </Col>
           </Row>
-          <button className="btn btn-outline-primary" onClick={this.refresh}> Indietro </button>
+          <Button onClick={this.refresh} outline color="dark"> Indietro </Button>
         </Card>
       </div>
     )
@@ -176,6 +230,3 @@ class infoStudente extends React.Component {
 }
 
 export default infoStudente
-
-
-
