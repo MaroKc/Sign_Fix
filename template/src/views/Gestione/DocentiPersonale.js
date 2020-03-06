d706d01a97b663e6eaa5876eeeb8da3f04f5b907
damn-fox <zonaniccolo92@gmail.com>
2020-03-06 16:31:52 +0100

    commit

---
+++ template/src/views/Gestione/DocentiPersonale.js
@@ -0,0 +1,216 @@
+import React, { Component } from 'react';
+import { Col, Row, Table, Card, CardHeader, CardBody, Button } from 'reactstrap';
+import { MDBDataTable } from 'mdbreact';
+import axios from 'axios'
+import {ToastsContainer, ToastsStore, ToastsContainerPosition} from 'react-toasts';
+
+
+
+
+
+class DocentiPersonale extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            docenti: [],
+            dettagliDocente: [],
+            lezioni: []
+        }
+
+    }
+
+    componentDidMount() {
+        this.getTeachers();
+        this.teacherDetails();
+        
+
+    }
+
+
+    getTeachers = () => {
+        axios.get('http://localhost:8080/listTeachers')
+            .then(res => {
+                const docenti = [];
+                res.data.data.map(item => docenti.push({
+                    firstName: item.first_name,
+                    lastName: item.last_name,
+                    emailDocente: item.email_responsible,
+                    companiesId: item.companies_id,
+                    hoursOfLessons: this.formatHours(item.hours_of_lessons)
+                }));
+                this.setState({ docenti });
+                this.getLesson();
+            })
+            .catch(err => console.error(err));
+    }
+
+
+
+
+    teacherDetails = () => {
+        axios.get('http://localhost:8080/teachersDetails')
+            .then(res => {
+                const dettagli = res.data.data;
+                var dettagliDocente = [];
+                dettagli.map(item =>
+                    dettagliDocente.push({
+                        companyName: item.company_name,
+                        lessonName: item.lesson,
+                        totalHours: this.formatHours(item.total_hours),
+                        companyId: item.company_id
+                    })
+                );
+                this.setState({ dettagliDocente: dettagliDocente })
+            })
+            .catch(err => console.error(err));
+    }
+
+    getLesson = () => {
+        let docente = this.state.docenti.find(docente => docente.emailDocente === 'michele@info.com')
+
+
+        axios.get('http://localhost:8080/lessonsTeacher/' + this.props.classe["id"] +"/" + docente.companiesId)
+            .then(res =>{
+                const lezioni = [];
+                res.data.map(item => lezioni.push({
+                    date: item.date,
+                    classroom: item.classroom,
+                    lessonName: item.lesson,
+                    startTime: this.formatHours(item.startTime),
+                    endTime: this.formatHours(item.endTime)
+                }));
+                this.setState({ lezioni });
+            })
+            .catch(err => console.error(err));
+    }
+
+    formatHours(hours) {
+        var startLessonAppoggio = (hours.toString()).split('.')
+        var startLesson = ''
+
+        if (startLessonAppoggio[1]) {
+            var startLessonSecondaParte = startLessonAppoggio[1].length == 1 ? startLessonAppoggio[1] + '0' : startLessonAppoggio[1]
+            startLesson = startLessonAppoggio[0] + ': ' + startLessonSecondaParte
+            return startLesson
+        }
+        else {
+            return startLessonAppoggio[0]
+        }
+    }
+
+    tabPane() {
+        const data = {
+            columns: [
+                {
+                    label: 'Data',
+                    field: 'date',
+                },
+                {
+                    label: 'Classe',
+                    field: 'classroom',
+                },
+                {
+                    label: 'Lezione',
+                    field: 'lessonName',
+                },
+                {
+                    label: 'Orario Inizio',
+                    field: 'startTime',
+                },
+                {
+                    label: 'Orario Fine',
+                    field: 'endTime',
+                }
+            ],
+            row: this.state.lezioni
+        };
+
+
+        return (
+            <div>
+                <CardHeader className="text-center font-weight-bold">
+                    <h5>Lezioni</h5>
+                </CardHeader>
+                <CardBody>
+                    <MDBDataTable
+                        responsive
+                        hover
+                        data={{ columns: data.columns , rows: data.row }}
+                        searching={false}
+                        paging={false}
+                        noBottomColumns={true}
+                    />
+                </CardBody>
+
+            </div>
+        );
+
+
+    }
+
+
+    infoTeacher() {
+
+        let docente = this.state.docenti.find(docente => docente.emailDocente === 'michele@info.com')
+        let dettaglioDocente = this.state.dettagliDocente.find(dettaglioDocente => dettaglioDocente.companyId === docente.companiesId)
+
+
+        return (
+            <div className="d-flex justify-content-center mt-5">
+                <Card className="w-75">
+                    <CardHeader className="text-center font-weight-bold">
+                        <h5>AREA PERSONALE</h5>
+                    </CardHeader>
+                    <CardBody>
+                        <Table borderless responsive>
+                            <tbody>
+                                <tr>
+                                    <td><h5>Nome:</h5></td>
+                                    <td><h5>{docente && docente.firstName}</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5>Cognome: </h5></td>
+                                    <td><h5 >{docente && docente.lastName}</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5>Nome azienda:</h5></td>
+                                    <td><h5>{dettaglioDocente && dettaglioDocente.companyName === 'michele@info.com' ? '' : dettaglioDocente && dettaglioDocente.companyName}</h5> </td>
+                                </tr>
+                                <tr>
+                                    <td><h5>Lezione:</h5></td>
+                                    <td><h5>{dettaglioDocente && dettaglioDocente.lessonName}</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5>Ore fatte:</h5></td>
+                                    <td><h5>{docente && docente.hoursOfLessons}</h5></td>
+                                </tr>
+                                <tr>
+                                    <td><h5>Ore di lezione:</h5></td>
+                                    <td><h5>{dettaglioDocente && dettaglioDocente.totalHours}</h5></td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        <Button color="success" size="lg" block> TIMBRA </Button>
+                    </CardBody>
+
+
+                {this.tabPane()}
+
+                </Card>
+
+
+            </div>
+        );
+    }
+
+
+
+
+    render() {
+        return this.infoTeacher()
+    }
+}
+
+export default DocentiPersonale;
