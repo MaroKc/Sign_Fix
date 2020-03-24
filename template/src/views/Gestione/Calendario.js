8e0b10cb209431bce8fc9cb5c1deed2be1e311e4
poldro <paolo.valmori@gmail.com>
2020-03-24 18:12:04 +0100

    bug

--- template/src/views/Gestione/Calendario.js
+++ template/src/views/Gestione/Calendario.js
@@ -26,7 +26,7 @@ class Calendario extends Component {
   importCalendar() {
     this.setState({ calendario: this.state.token });
     this.toggle();
-    axios.post('http://localhost:8080/calendar/importLessons', { email: 'daniele.marocchi.studio@fitstic-edu.com', token: this.state.token, ccorso: this.classe.id})
+    axios.post('http://localhost:8080/calendar/importLessons', { email: 'daniele.marocchi.studio@fitstic-edu.com', token: this.state.token, corso: this.classe.id})
       .then(res => {
         console.log(res);
         console.log(res.data);
