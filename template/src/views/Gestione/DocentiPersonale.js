a97e93c21b4ff52675fecb65576f3675bbb4e025
DelfagioF <francesco.delfagio.studio@fitstic-edu.com>
2020-03-27 11:12:10 +0100

    update inutile

--- template/src/views/Gestione/DocentiPersonale.js
+++ template/src/views/Gestione/DocentiPersonale.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {Table, Card, CardHeader, CardBody, Button, Input, Row, Col } from 'reactstrap';
-import { MDBDataTable } from 'mdbreact';
+import { Table, Card, CardHeader, CardBody,  Input, Row, Col } from 'reactstrap';
+import { MDBDataTable, MDBBtn } from 'mdbreact';
 import axios from 'axios'
 import { ToastsContainer, ToastsStore, ToastsContainerPosition } from 'react-toasts';
 
@@ -17,9 +17,9 @@ class DocentiPersonale extends Component {
             lezioni: [],
             docenti: [],
             dettagliDocente: [],
-            value:'',
+            value: '',
             changeState: false,
-            
+
         }
     }
 
@@ -28,7 +28,7 @@ class DocentiPersonale extends Component {
     componentDidMount() {
         this.getTeachers();
         this.getTeacherDetails();
-    console.log(this.state.changeState)
+        console.log(this.state.changeState)
     }
 
 
@@ -103,7 +103,7 @@ class DocentiPersonale extends Component {
                     lessonName: item.lesson,
                     startTime: this.formatHours(item.startTime),
                     endTime: this.formatHours(item.endTime),
-                    percentage: item.percentuale.toFixed(0)+"%"
+                    percentage: item.percentuale.toFixed(0) + "%"
                 }));
                 this.setState({ lezioni });
             })
@@ -155,10 +155,10 @@ class DocentiPersonale extends Component {
         let tipoLezione = this.state.lezioni.filter(item => item.lessonName === this.state.value)
         var sum = tipoLezione.reduce(
             (accumulator, currentValue) => accumulator + parseInt(currentValue.percentage.split("%"), 10)
-            ,initialValue
+            , initialValue
         );
-        
-        return (sum / tipoLezione.length).toFixed(0)+"%"
+
+        return (sum / tipoLezione.length).toFixed(0) + "%"
     }
 
     formattedDate = (d) => {
@@ -166,12 +166,12 @@ class DocentiPersonale extends Component {
         let month = String(date.getMonth() + 1);
         let day = String(date.getDate());
         const year = String(date.getFullYear());
-      
+
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
-      
+
         return `${day}/${month}/${year}`;
-      }
+    }
 
 
     selectLesson = () => {
@@ -208,7 +208,7 @@ class DocentiPersonale extends Component {
                                 {this.state.dettagliDocente.map((item, i) => <option key={i} value={item.lessonName}>{item.lessonName}</option>)}
                             </Input>
                         </th> */}
-                    <div className="d-flex justify-content-start mt-3 ml-3 mb-3">{this.state.dettagliDocente.map((item, i) => <Button value={item.lessonName} color={this.state.value === item.lessonName ? 'primary' : 'ghost-primary'} className={this.state.value === item.lessonName ? 'disabled active btn btn-pill mr-3 h-50' : 'btn btn-pill mr-3 h-50'} onClick={(e) => this.setState({ value: e.target.value })} key={i}>{item.lessonName}</Button>)}</div>
+                    <div className="d-flex justify-content-start mt-3 ml-3 mb-3">{this.state.dettagliDocente.map((item, i) => <MDBBtn value={item.lessonName} color={this.state.value === item.lessonName ? 'primary' : 'ghost-primary'} className={this.state.value === item.lessonName ? 'disabled active btn btn-pill mr-3 h-50' : 'btn btn-pill mr-3 h-50'} onClick={(e) => this.setState({ value: e.target.value })} key={i}>{item.lessonName}</MDBBtn>)}</div>
                     <Row>
                         <Col xs="6" className="mt-4 mb-4 text-center">
                             <h6 className="mb-2">Ore fatte:</h6>
@@ -226,7 +226,7 @@ class DocentiPersonale extends Component {
 
     }
 
-    
+
 
 
     pastLessons() {
@@ -268,26 +268,26 @@ class DocentiPersonale extends Component {
             row: pastLesson
         };
 
-        if(this.state.changeState){
-        return (
-            <div className="mb-4">
-                <CardBody>
-                    <MDBDataTable
-                        responsive
-                        hover
-                        data={{ columns: data.columns, rows: data.row }}
-                        searching={false}
-                        paging={false}
-                        noBottomColumns={true}
-                    />
-                </CardBody>
+        if (this.state.changeState) {
+            return (
+                <div className="mb-4">
+                    <CardBody>
+                        <MDBDataTable
+                            responsive
+                            hover
+                            data={{ columns: data.columns, rows: data.row }}
+                            searching={false}
+                            paging={false}
+                            noBottomColumns={true}
+                        />
+                    </CardBody>
 
-            </div>
-        );
-        }   
+                </div>
+            );
+        }
     }
 
-    
+
 
     futureLessons() {
         var d = new Date(),
@@ -329,22 +329,23 @@ class DocentiPersonale extends Component {
             ],
             row: futureLesson
         };
-        if(futureLesson.length !== 0 && !this.state.changeState){
-        return (
-            <div className="mb-4">
-                <CardBody>
-                    <MDBDataTable
-                        responsive
-                        hover
-                        data={{ columns: data.columns, rows: data.row }}
-                        searching={false}
-                        paging={false}
-                        noBottomColumns={true}
-                    />
-                </CardBody>
+        if (futureLesson.length !== 0 && !this.state.changeState) {
+            return (
+                <div className="mb-4">
+                    <CardBody>
+                        <MDBDataTable
+                            responsive
+                            hover
+                            data={{ columns: data.columns, rows: data.row }}
+                            searching={false}
+                            paging={false}
+                            noBottomColumns={true}
+                        />
+                    </CardBody>
 
-            </div>
-        )}else if (!this.state.changeState){return <h3 className="mb-4 text-center">A quanto pare non hai lezioni in programma!</h3>}
+                </div>
+            )
+        } else if (!this.state.changeState) { return <h3 className="mb-4 text-center">A quanto pare non hai lezioni in programma!</h3> }
     }
 
 
@@ -367,52 +368,52 @@ class DocentiPersonale extends Component {
         let mese = data.getMonth();
 
 
-        
-        if(giorno == 0) giorno = "Domenica";
-        if(giorno == 1) giorno = "Lunedì";
-        if(giorno == 2) giorno = "Martedì";
-        if(giorno == 3) giorno = "Mercoledì";
-        if(giorno == 4) giorno = "Giovedì";
-        if(giorno == 5) giorno = "Venerdì";
-        if(giorno == 6) giorno = "Sabato";
-        
-        if(mese == 0) mese = "Gennaio";
-        if(mese == 1) mese = "Febbraio";
-        if(mese == 2) mese = "Marzo";
-        if(mese == 3) mese = "Aprile";
-        if(mese == 4) mese = "Maggio";
-        if(mese == 5) mese = "Giugno";
-        if(mese == 6) mese = "Luglio";
-        if(mese == 7) mese = "Agosto";
-        if(mese == 8) mese = "Settembre";
-        if(mese == 9) mese = "Ottobre";
-        if(mese == 10) mese = "Novembre";
-        if(mese == 11) mese = "Dicembre";
+
+        if (giorno == 0) giorno = "Domenica";
+        if (giorno == 1) giorno = "Lunedì";
+        if (giorno == 2) giorno = "Martedì";
+        if (giorno == 3) giorno = "Mercoledì";
+        if (giorno == 4) giorno = "Giovedì";
+        if (giorno == 5) giorno = "Venerdì";
+        if (giorno == 6) giorno = "Sabato";
+
+        if (mese == 0) mese = "Gennaio";
+        if (mese == 1) mese = "Febbraio";
+        if (mese == 2) mese = "Marzo";
+        if (mese == 3) mese = "Aprile";
+        if (mese == 4) mese = "Maggio";
+        if (mese == 5) mese = "Giugno";
+        if (mese == 6) mese = "Luglio";
+        if (mese == 7) mese = "Agosto";
+        if (mese == 8) mese = "Settembre";
+        if (mese == 9) mese = "Ottobre";
+        if (mese == 10) mese = "Novembre";
+        if (mese == 11) mese = "Dicembre";
 
         const todayLesson = this.state.lezioni.filter(lezione => lezione.date === currDate)
 
         if (todayLesson.length !== 0) {
             return (
                 <Card>
-                <CardHeader className="my-auto text-center">
-                   <h4> <b>{giorno + ' ' + day + ' ' + mese + ' ' + year}</b></h4>
-                </CardHeader>
-                <div>   
-                    <CardBody>
-                    <Button color="success" size="lg" className="mb-3" block> TIMBRA </Button>
-                    <h3 className="text-center">{todayLesson['0'].startTime} - {todayLesson['0'].endTime}</h3>
-                    <h5 className="text-center">{todayLesson['0'].classroom}, {todayLesson['0'].lessonName}</h5>
-                    
-                    </CardBody>
-                </div>
-            </Card>
+                    <CardHeader className="my-auto text-center">
+                        <h4> <b>{giorno + ' ' + day + ' ' + mese + ' ' + year}</b></h4>
+                    </CardHeader>
+                    <div>
+                        <CardBody>
+                            <MDBBtn color="success" size="lg" className="mb-3" block> TIMBRA </MDBBtn>
+                            <h3 className="text-center">{todayLesson['0'].startTime} - {todayLesson['0'].endTime}</h3>
+                            <h5 className="text-center">{todayLesson['0'].classroom}, {todayLesson['0'].lessonName}</h5>
+
+                        </CardBody>
+                    </div>
+                </Card>
             )
-        }else{
+        } else {
             return (
                 <Card>
-                <CardHeader className="my-auto text-center">
-                   <h4> <b>{giorno + ' ' + day + ' ' + mese + ' ' + year}</b></h4>
-                </CardHeader>
+                    <CardHeader className="my-auto text-center">
+                        <h4> <b>{giorno + ' ' + day + ' ' + mese + ' ' + year}</b></h4>
+                    </CardHeader>
                 </Card>
             )
         }
@@ -427,24 +428,25 @@ class DocentiPersonale extends Component {
                 <div>
                     {this.todayLesson()}
                     <div className="text-center mb-4">
-                    <h3>{docente && docente.firstName} {docente && docente.lastName}</h3>
-                    <h5>{docente && docente.emailDocente}</h5>
-                </div>
-           
-                   
-                <hr />
-                            {this.selectLesson()}
-                     
-         
-              
+                        <h3>{docente && docente.firstName} {docente && docente.lastName}</h3>
+                        <h5>{docente && docente.emailDocente}</h5>
+                    </div>
+
+
+                    <hr />
+                    {this.selectLesson()}
+
+
+
                 </div>
                 <hr />
                 <div className="d-flex justify-content-around mb-4">
-                    {this.state.changeState ? <Button disabled={true} color="primary" onClick={this.changeState}> <h5>Lezioni Passate</h5></Button> : <Button outline color="primary" onClick={this.changeState}> <h5>Lezioni Passate</h5></Button>}
+                    {this.state.changeState ? <MDBBtn disabled color="primary" onClick={this.changeState}> <h5>Lezioni Passate</h5></MDBBtn> : <MDBBtn color="primary" onClick={this.changeState}> <h5>Lezioni Passate</h5></MDBBtn>}
 
-                    {!this.state.changeState ? <Button disabled={true} color="primary" onClick={this.changeState}> <h5>Lezioni Future</h5></Button> : <Button outline color="primary" onClick={this.changeState}> <h5>Lezioni Future</h5></Button>}
+                    {!this.state.changeState ? <MDBBtn disabled={true} color="primary" onClick={this.changeState}> <h5>Lezioni Future</h5></MDBBtn> : <MDBBtn outline color="primary" onClick={this.changeState}> <h5>Lezioni Future</h5></MDBBtn>}
                 </div>
                 
+
                 {this.futureLessons()}
                 {this.pastLessons()}
             </div>
